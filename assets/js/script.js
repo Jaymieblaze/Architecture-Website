d69f8a5dd806a6c1e6f595d5020aa48d9c35fd19
Jaymieblaze <jameseyaiaro@gmail.com>
2024-11-25 13:40:12 +0100

    Swiper- JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,3 +26,24 @@ const linkAction = () =>{
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+// SWIPER
+const swiperHome = new Swiper('.home__swiper', {
+    loop: true,
+    speed: 800,
+    parallax: true,
+    effect: 'fade',
+  
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'fraction',
+
+      formatFractionCurrent: (number) => {return '0' + number},
+      formatFractionTotal: (number) => {return '0' + number}
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
